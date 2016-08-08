bb2900f631672ea8fd35be0efcb6b5d77c36a9fa
Kwang-Hyun Chung <kwhy1017@gmail.com>
2016-08-08 19:35:49 +0900

    Squashed 'memo/' changes from ea45d25..0a206c6
    0a206c6 add test7
    bb8783f add test6
    0e1575e add test5
    4201a60 Merge branch 'master' of https://github.com/kwhy1017/sub_repo
    e72c800 add test4
    a0fb2b6 add test3
    04d28d6 add test3
    27fcc09 add test2
    2df26c6 add test1
    7549e10 remove test~
    
    git-subtree-dir: memo
    git-subtree-split: 0a206c65c741dd73ec714308ca082af597dfcaa6

--- memo.c
+++ memo.c
@@ -2,17 +2,12 @@
 
 int memo(){
 	printf("function memo.\n");
-	printf("test\n");
-	printf("test1\n");
-	printf("test2\n");
-	printf("test3\n");
-	printf("test4 from test\n");
-	printf("test5 from memo\n");
-	printf("test6 from test\n");
-	printf("test7 from memo\n");
-	printf("test8 from test\n");
-	printf("test9 from memo\n");
-	printf("test10 from memo\n");
-	printf("test11 from memo\n");
+	printf("test.\n");
+	printf("test2.\n");
+	printf("test3.\n");
+	printf("test4.\n");
+	printf("test5.\n");
+	printf("test6.\n");
+	printf("test7.\n");
 	return 0;
 }
